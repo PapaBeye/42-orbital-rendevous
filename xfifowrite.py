81b3597f8e03426ce8bce8895e73b3d48828d1bc
hudginspj <hudginspj@mymail.vcu.edu>
2017-09-17 01:10:08 -0400

    working on IPC for commands

--- xfifowrite.py
+++ xfifowrite.py
@@ -5,13 +5,13 @@ def writeline(line):
     with open("cmdfifo", "w") as f:
         f.write(line + '\n')
         print('wrote')
-        time.sleep(3)
+        time.sleep(2)
 
-writeline("1.0")
-writeline("2.0")
-writeline("3.0")
-writeline("4.0")
-writeline("-1.0")
+writeline("1.0 c")
+writeline("2.0 c")
+writeline("3.0 c")
+# writeline("4.0 c")
+writeline("-1.0 c")
 print("done")
 
 
